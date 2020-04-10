a0502cf8b03437f427372e770b2d7c3b3e6cc32f
iyengaraditya <56295215+iyengaraditya@users.noreply.github.com>
2020-04-10 15:51:18 +0530

    Plot 3

---
+++ plot3.R
@@ -0,0 +1,16 @@
+download.file("https://d396qusza40orc.cloudfront.net/exdata%2Fdata%2Fhousehold_power_consumption.zip",
+              "power.zip", method = "curl")
+unzip("power.zip")
+power <- read.table("household_power_consumption.txt", header=TRUE, sep=";", stringsAsFactors=FALSE, dec=".")
+newpower <- power[power$Date %in% c("1/2/2007","2/2/2007") ,]
+datesandtimes <- strptime(paste(newpower$Date, newpower$Time), "%d/%m/%Y %H:%M:%S") 
+gapower <- as.numeric(newpower$Global_active_power)
+sub1 <- as.numeric(newpower$Sub_metering_1)
+sub2 <- as.numeric(newpower$Sub_metering_2)
+sub3 <- as.numeric(newpower$Sub_metering_3)
+png("plot3.png", width=480, height=480)
+plot(datesandtimes, sub1, type="l", ylab="Energy sub metering", xlab="")
+lines(datesandtimes, sub2, type="l", col="red")
+lines(datesandtimes, sub3, type="l", col="blue")
+legend("topright", c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"), lty=1, lwd=2.5, col=c("black", "red", "blue"))
+dev.off()
